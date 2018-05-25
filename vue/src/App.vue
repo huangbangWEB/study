d1bcc77d68ca24c7f18f0a856daef9c9a74e106f
huangbangWEB <a851571078@gmail.com>
2018-05-25 16:52:29 +0800

    6

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,40 @@
 <!-- 以后项目的根组件 -->
 <template>
 	<div>
-		<!-- 1.0 template主要是放html元素的（html的页面结构） -->
-		<span v-text="msg" class="red"></span>
+		<!-- 1.0 利用mint-ui中的header组件实现整个系统的头部 -->
+		<mt-header fixed title="传智播客.黑马程序员Vue商城系统"></mt-header>
+
+		<!-- 2.0 利用vue-router的 <router-view>进行占位 -->
+		<router-view></router-view>
+
+		<!-- 3.0 利用mui中的tabbar组件实现系统的底部 -->
+		<nav class="mui-bar mui-bar-tab">
+			<router-link class="mui-tab-item " to="/home">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/tabbar-with-chat">
+				<span class="mui-icon mui-icon-email">
+				
+				</span>
+				<span class="mui-tab-label">会员</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/shopcar">
+				<span class="mui-icon mui-icon-contact">
+					<span class="mui-badge">9</span>
+				</span>
+				<span class="mui-tab-label">购物车</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">搜索</span>
+			</router-link>
+		</nav>
 	</div>
 </template>
 
 <script>
+	import { Toast } from 'mint-ui';
 	// 负责导出 .vue这个组件对象(它本质上是一个Vue对象,所以Vue中该定义的元素都可以使用)
 	export default{  // es6的导出对象的写法
 		data:function(){  //等价于 es5的 data:function(){
@@ -15,7 +43,9 @@
 			}
 		},
 		methods:{
-			
+			tip:function(){
+				Toast('你好');
+			}
 		},
 		created(){
 
---
+++ vue/src/components/home.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="tmpl">
+	 <!-- 1.0 轮播图：mint-ui中的 swipe组件实现 -->
+		 <mt-swipe :auto="2000">
+		  <!-- <mt-swipe-item>1</mt-swipe-item>
+		   -->
+			  <mt-swipe-item v-for='item in list'> <img :src="item"/></mt-swipe-item>
+			
+		</mt-swipe>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+		return {
+			list:['./statics/imgs/lunbo1','../../statics/imgs/lunbo2','../../statics/imgs/lunbo3']
+			}
+		},
+		created(){
+			
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style>
+	
+</style>
